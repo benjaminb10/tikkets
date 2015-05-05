a217c4811641b97fd4aa113bfd66c58038a63443
Benjamin Benoudis <benoudis.benjamin@gmail.com>
2015-05-05 14:56:27 +0200

    Fix admin edit view

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :admin
 #
 # or
 #
@@ -13,5 +13,13 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :admin
+    end
+
+    f.actions
+  end
 
 end
